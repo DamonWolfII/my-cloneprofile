ab67f51b16d662c9dd12491dbd2226cf41870bff
DamonWolfII <utsav2071@outlook.com>
2024-01-08 10:41:50 +0545

    7th commit

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -9,12 +9,44 @@ export default function Resume(){
             <div className="pt-12">
                 <span className="left-1/2 w-[1px] h-full bg-blue-600 block t-0 absolute"></span>
                 <div className="flex flex-wrap -mx-4">
-                    <div className="w-1/2 px-4">
+                    <div className="w-1/2 px-4 ">
                         <div className="text-right px-0 mr-5 pb-12 relative">
                             <span className=" left-auto w-4 h-4 bg-white border-4 border-blue-600 rounded-full top-10 absolute right-[-2.75rem]"></span>
                             <h3 className="font-bold text-xl mb-1 m-0 block pt-10">UI/UX Designer</h3>
                             <p className="font-bold text-base mb-3 m-0 text-gray-400">Google INC | New York</p>
-                            <span className="font-bold text-xs py-2 px-4 rounded-full justify-center items-center">2011-2013</span>
+                            <span className="font-bold text-xs py-2 px-4 rounded-full justify-center items-center bg-blue-600 text-white">2011-2013</span>
+                            <p className="font-medium text-base m-0 text-gray-400 block pt-3 text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore et dolore magna aliqua Loremipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                    <div className="w-1/2 px-4 "></div>
+                    <div className="w-1/2 px-4 "></div>
+                    <div className="w-1/2 px-4">
+                        <div className="px-0 ml-5 pb-12 relative w-full text-left">
+                            <span className=" w-4 h-4 bg-white border-4 border-blue-600 rounded-full top-10 absolute left-[-2.75rem] "></span>
+                            <h3 className="font-semibold text-xl mb-1 m-0 block pt-10">Specialization Course</h3>
+                            <p className="font-bold text-base mb-3 m-0 text-gray-400">Standford University | New York</p>
+                            <span className="font-bold text-xs py-2 px-4 rounded-full justify-center items-center bg-blue-600 text-white">2017-2018</span>
+                            <p className="font-medium text-base m-0 text-gray-400 block pt-3 text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore et dolore magna aliqua Loremipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                    <div className="w-1/2 px-4 ">
+                        <div className="text-right px-0 mr-5 pb-12 relative">
+                            <span className=" left-auto w-4 h-4 bg-white border-4 border-blue-600 rounded-full top-10 absolute right-[-2.75rem]"></span>
+                            <h3 className="font-bold text-xl mb-1 m-0 block pt-10">Web Designer</h3>
+                            <p className="font-bold text-base mb-3 m-0 text-gray-400">Facebook INC | New York</p>
+                            <span className="font-bold text-xs py-2 px-4 rounded-full justify-center items-center bg-blue-600 text-white">2014-2016</span>
+                            <p className="font-medium text-base m-0 text-gray-400 block pt-3 text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore et dolore magna aliqua Loremipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                    <div className="w-1/2 px-4 "></div>
+                    <div className="w-1/2 px-4 "></div>
+                    <div className="w-1/2 px-4">
+                        <div className="px-0 ml-5 pb-12 relative w-full text-left">
+                            <span className=" w-4 h-4 bg-white border-4 border-blue-600 rounded-full top-10 absolute left-[-2.75rem] "></span>
+                            <h3 className="font-semibold text-xl mb-1 m-0 block pt-10">Bachelor Degree</h3>
+                            <p className="font-bold text-base mb-3 m-0 text-gray-400">Standford University | New York</p>
+                            <span className="font-bold text-xs py-2 px-4 rounded-full justify-center items-center bg-blue-600 text-white">2019-2021</span>
+                            <p className="font-medium text-base m-0 text-gray-400 block pt-3 text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore et dolore magna aliqua Loremipsum dolor sit amet, consectetur adipisicing elit.</p>
                         </div>
                     </div>
                 </div>
